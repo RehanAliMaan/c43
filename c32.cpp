03c84fb94b7106405892e8eda9ce58bb184bf830
Rehan Ali maan <130174371+RehanAliMaan@users.noreply.github.com>
2023-12-06 20:16:50 +0500

    Add files via upload

---
+++ c32.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n,a,p=1;
+	cout<<"enter the number";
+	cin>>n;
+	a=n;
+	while(n>0)
+	{
+		p=n*p;
+		n__;
+		
+	}
+	cout<<"factorail of"<<a<<"is"<<p;
+	return 0;
+}
